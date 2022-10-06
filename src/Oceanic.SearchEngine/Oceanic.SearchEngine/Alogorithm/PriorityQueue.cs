bb2e930c8908a32f91c398564946cebb692a43d8
Karol Strzelecki <k-strzelecki@poczta.pl>
2022-10-06 10:03:59 +0200

    First version of djikstra

---
+++ src/Oceanic.SearchEngine/Oceanic.SearchEngine/Alogorithm/PriorityQueue.cs
@@ -0,0 +1,41 @@
+﻿namespace Oceanic.SearchEngine.Alogorithm
+{
+    class PriorityQueue : LinkedList<Node>
+    {
+        public void AddNodeWithPriority(Node node)
+        {
+            if (this.Count == 0)
+            {
+                this.AddFirst(node);
+            }
+            else
+            {
+                if (node.Value >= this.Last.Value.Value)
+                {
+                    this.AddLast(node);
+                }
+                else
+                {
+                    for (LinkedListNode<Node> it = this.First; it != null; it = it.Next)
+                    {
+                        if (node.Value <= it.Value.Value)
+                        {
+                            this.AddBefore(it, node);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool HasLetter(string letter)
+        {
+            for (LinkedListNode<Node> it = this.First; it != null; it = it.Next)
+            {
+                if (it.Value.Name == letter) { return true; }
+            }
+            return false;
+        }
+    }
+}
+}
